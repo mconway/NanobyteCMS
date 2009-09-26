acdaff2e7750a9cde7954baa2ebdc89e30b795b1
Mike Conway <mike.conway@wiredbyte.com>
2009-09-26 00:34:53 +0000

    Made progress on Mod_UrlAlias

--- nanobyte/modules/urlalias/mod_urlalias.php
+++ nanobyte/modules/urlalias/mod_urlalias.php
@@ -5,6 +5,7 @@ class Mod_UrlAlias{
 	
 	public function __construct($id=null){
 		$this->dbh = DBCreator::GetDbObject();
+		$this->id = $id;
 	}
 	
 	public function checkAlias($alias){
@@ -19,6 +20,20 @@ class Mod_UrlAlias{
 			$query->execute(array(0=>$alias."%"));
 			return false;
 		}
+	}
+	
+	public function commit(){
+		if(!isset($this->id)){
+			$query = $this->dbh->prepare("INSERT INTO ".DB_PREFIX."_url_alias (`alias`,`path`) VALUES(:alias,:path)");
+		}else{
+			$query = $this->dbh->prepare("UPDATE ".DB_PREFIX."_url_alias SET `alias`=:alias, `path`=:path");	
+		}
+		$query->execute(array(':alias'=>$this->alias,':path'=>$this->path));
+	}
+	
+	public function delete(){
+		$query = $this->dbh->prepare("DELETE FROM ".DB_PREFIX."_url_alias WHERE id=:id");
+		$query->execute(array(':id'=>$this->id));
 	}
 	
 	public static function Install(){
@@ -54,23 +69,77 @@ class Mod_UrlAlias{
 
 class UrlAliasController extends BaseController{
 	
-	public static function Admin(){
+	public static function addAlias(){
+		$form = new HTML_QuickForm('newuser','post','user/register/');
+		//create form elements
+		$form->addElement('header','','Create New Alias');
+		$form->addElement('text', 'alias', 'Alias', array('size'=>25, 'maxlength'=>15));
+		$form->addElement('text', 'path', 'Actual Path', array('size'=>25, 'maxlength'=>50));
+		$form->addElement('submit', 'submit', 'Submit');
+		//apply form prefilters
+		$form->applyFilter('__ALL__', 'trim');
+		$form->applyFilter('__ALL__', 'strip_tags');
+		//add form rules
+		$form->addRule('alias', 'A valid alias is required.', 'required');
+		$form->addRule('path', 'A valid path is required.', 'required');
+		//If the form has already been submitted - validate the data
+		if($_POST['submit']){
+			if($form->validate()){
+				
+			}
+		}
+
+		//send the form to smarty
+		return $form->toArray(); 
+	}
+	
+	public static function admin(){
+		$content = '';
 		$Core = BaseController::getCore();
-		if(isset($Core->args[1]) && function_exists($Core->args[1])){
+		if(isset($Core->args[1]) && method_exists('UrlAliasController',$Core->args[1])){
 			$content = call_user_func(array('self',$Core->args[1]));
 		}else{
-			$tabs = array(Core::l('Aliases','admin/urlalias/list'));
-			$smarty->assign('tabs',$tabs);
-			if($ajax){$jsonObj->tabs = $smarty->fetch('tabs.tpl');}
+			$tabs = array(Core::l('Aliases','admin/urlalias/getList'));
+			$Core->smarty->assign('tabs',$tabs);
+			if($Core->ajax){$Core->json_obj->tabs = $Core->smarty->fetch('tabs.tpl');}
 		}
-		$Core->jsonObj->content = $content;
+		$Core->json_obj->content = $content;
 	}
 
-	public static function AddAlias(){
+	public static function delete(){
 		
 	}
 
-	public static function getList($page){
+	public static function edit(){
+		$Core = parent::getCore();
+		$Core->smarty->assign('form',self::form('edit'));
+		return $Core->smarty->fetch('form.tpl');
+	}
+
+	public static function form($func){
+		$Core = parent::getCore();
+		$form = new HTML_QuickForm('urlalias','post','admin/urlalias/'.$func);
+		if($func == 'edit'){
+			$form->setDefaults(array(
+				'alias'=>'',
+				'realpath'=>''
+			));
+		}
+		$header = $func == 'add' ? 'Create Alias' : 'Edit Alias';
+		$form->addElement('header','',$header);
+		$form->addElement('text', 'alias', 'Alias', array('size'=>62, 'maxlength'=>80));
+		$form->addElement('text', 'realpath', 'Real Path', array('size'=>62, 'maxlength'=>80));
+		$form->addElement('submit','submit','Submit');
+		if(isset($_POST['submit']) && $form->validate()){
+			$form->process(array('UrlAliasController','Save'));
+			return;
+		}
+		return $form->toArray();
+	}
+
+	public static function getList(){
+		$Core = parent::getCore();
+		$page = isset($Core->args[2]) ? $Core->args[2] : 1;
 		$alias = new Mod_UrlAlias();
 		$alias->Read(parent::GetStart($page,10), 10); //array of objects
 		$list = array();
@@ -79,13 +148,14 @@ class UrlAliasController extends BaseController{
 			'class' => 'action-link-tab',
 			'title' => 'Editing Alias'
 		);
+		$actions = '';
 		foreach ($alias->all['items'] as $key=>$a){
 			$actions .= Core::l('info','admin/user/edit/'.$a['id'],$options);
 			array_push($list, array(
 				'id'=>$a['id'], 
 				'alias'=>$a['alias'], 
 				'real path'=>$a['path'], 
-				'actions'=>Core::l('edit','admin/urlalias/'.$a['id'],$options)
+				'actions'=>Core::l('edit','admin/urlalias/edit/'.$a['id'],$options)
 			));
 		}
 		//create the actions options
@@ -109,39 +179,24 @@ class UrlAliasController extends BaseController{
 			'extra'=>$extra,
 			'list'=>$list
 		);
-		return $smartyArray;
+		$Core->smarty->assign($smartyArray);
+		return $Core->smarty->fetch('list.tpl');
 	}
 
-	public static function list($page=1){
-		$Core = BaseController::getCore();
-		$Core->smarty->assign(self::GetList($page));
-		return $Core->smarty->fetch('list.tpl');	
+	public static function save($params){
+		$Core = parent::getCore();
+		
+		$alias = new Mod_UrlAlias();
+		$alias->alias = $params['alias'];
+		$alias->path = $params['realpath'];
+		$alias->commit();
+		 
+		$Core->json_obj->callback = 'nanobyte.closeParentTab';
+		$Core->json_obj->args = 'input[name=submit][value=Submit]';
+
+		return;
 	}
 
-
-	public static function AddAlias(){
-		$form = new HTML_QuickForm('newuser','post','user/register/');
-		//create form elements
-		$form->addElement('header','','Create New Alias');
-		$form->addElement('text', 'alias', 'Alias', array('size'=>25, 'maxlength'=>15));
-		$form->addElement('text', 'path', 'Actual Path', array('size'=>25, 'maxlength'=>50));
-		$form->addElement('submit', 'submit', 'Submit');
-		//apply form prefilters
-		$form->applyFilter('__ALL__', 'trim');
-		$form->applyFilter('__ALL__', 'strip_tags');
-		//add form rules
-		$form->addRule('alias', 'A valid alias is required.', 'required');
-		$form->addRule('path', 'A valid path is required.', 'required');
-		//If the form has already been submitted - validate the data
-		if($_POST['submit']){
-			if($form->validate()){
-			
-			}
-		}
-
-		//send the form to smarty
-		return $form->toArray(); 
-	}
 }
 
 ?>
